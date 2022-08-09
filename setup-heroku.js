c7219247bb88ca9edd836ebfb868b2e1409c8027
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-09 11:05:24 -0700

    refactor setup-heroku file

--- setup-heroku.js
+++ setup-heroku.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-console */
 const pool = require('./lib/utils/pool');
 const { readFileSync } = require('node:fs');
 const sql = readFileSync('./sql/setup.sql', 'utf-8');
 
 async function setupDb() {
   try {
-    const response = await pool.query(sql);
-    console.log(response);
+    await pool.query(sql);
+    console.log('setup complete');
   } catch (e) {
     console.log(e);
   }
